2fe866a951235ab39becebe626b1c6a583357d4c
DmitryHash <podolnitsky@gmail.com>
2023-05-02 15:11:10 +0300

    add component: UserInfo

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,12 @@
 import './App.scss';
 import { Button } from './components/Button/Button';
+import { UserInfo } from './components/UserInfo/UserInfo';
 
 export const App = () => {
 
   return (
     <div>
+      <UserInfo username='Dmitry Podolnitski' />
     </div>
   );
 }
---
+++ src/components/UserInfo/UserInfo.tsx
@@ -0,0 +1,21 @@
+import { FC } from "react";
+import './UserInfo.scss'
+
+interface IUserInfo {
+    username: string
+}
+
+export const UserInfo: FC<IUserInfo> = ({username}) => {
+
+const generateInintials = (username: string) => {
+   return username.split(' ').map((e) => e[0]).join('').toUpperCase();
+}
+
+
+    return (
+        <div className='userinfo'>
+            <div className="userinfo__initials">{generateInintials(username)}</div>
+            <div className="userinfo__name">{username}</div>
+        </div>
+    )
+}
