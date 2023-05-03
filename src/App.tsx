1c61621f2256eed2cf9416dd52cf2eaeaf2b131b
DmitryHash <podolnitsky@gmail.com>
2023-05-03 13:31:24 +0300

    delete 2 and 5 string

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import './App.scss';
-import { Button } from './components/Button/Button';
 import { Hamburger } from './components/Hamburger/Hamburger';
 import { Title } from './components/Title/Title';
-import { UserInfo } from './components/UserInfo/UserInfo';
 
 export const App = () => {
 
   return (
     <div>
       <Title />
-      <UserInfo username='Dmitry Podolnitski' />
       <Hamburger />
     </div>
   );
