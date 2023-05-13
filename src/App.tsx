69389d2c9e064ec6c1185e5567cba038ba15bd4d
DmitryHash <podolnitsky@gmail.com>
2023-05-13 11:26:42 +0300

    do content page for end

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,7 @@
 import { PageTemplate } from "./components/PageTemplate/PageTemplate";
 import { ContentPage } from "./pages/ContentPage/ContentPage";
-import { RegConfirm } from "./pages/RegConfirm/RegConfirm";
-import { SignIn } from "./pages/SignIn/SignIn";
-// import { RegistrationConfirm } from "./pages/RegistrationConfirm/RegistrationConfirm";
-import { SignUp } from "./pages/SignUp/SignUp";
-import { Success } from "./pages/Success/Success";
+
+
 
 export const App = () => {
 
--- src/pages/ContentPage/ContentPage.tsx
+++ src/pages/ContentPage/ContentPage.tsx
@@ -1,15 +1,83 @@
 import { FC } from 'react';
-import './ContentPage.scss';
 import { Typography } from '../../components/Typography/Typography';
+import { cards } from '../../mock/cards'
+import './ContentPage.scss';
+import { IconButton } from '../../components/IconButton/IconButton';
+import thumbsUp from '../../assets/icons/thumbsUp.svg'
+import thumbsDown from '../../assets/icons/thumbsDown.svg'
+import favorites from '../../assets/icons/favorites.svg'
+import arrowPrev from '../../assets/icons/arrowPrev.svg'
+import arrowNext from '../../assets/icons/arrowNext.svg'
 
 interface IContentPage {
 }
 
 export const ContentPage: FC<IContentPage> = () => {
+    const { title, image } = cards[0];
+
+
     return (
-        <div className='content-page'>
-            <Typography content='Astronauts prep for new solar arrays on nearly seven-hour spacewalk' type='H1'/>
-            
-        </div>
+        <>
+            <div className='content-page'>
+                <Typography content={title} type='H1' />
+                <div className='content-page__img-box'>
+                    <img src={image} alt={title} className='content-page__img' />
+                </div>
+                <div className='content-page_content'>
+                    <Typography content='Astronauts Kayla Barron and Raja Chari floated out of the International Space Station airlock for a spacewalk Tuesday, installing brackets and struts to support new solar arrays to upgrade the research lab’s power system on the same day that crewmate Mark Vande Hei marked his 341st day in orbit, a U.S. record for a single spaceflight.' type='textPrimary' />
+                    <Typography content='During the final days of Alice Neel’s blockbuster solo show at the Metropolitan Museum of Art this summer, the line into the exhibition spanned the length of the museum’s European paintings corridor, and the wait was over half an hour. Titled “People Come First,” the show featured more than 100 gritty cityscapes, domestic interiors, and stripped-down portraits of Neel’s neighbors, friends, and fellow artists in the largest-ever showing of her work in her hometown of New York City.' type='textPrimary' />
+                    <Typography content='The stories tracked Hambling’s trailblazing career while focusing on her current and upcoming projects. The artist’s installation Relic (2021), produced alongside sound recordist Chris Watson, was recently on view at Snape Maltings in London. Meanwhile, this October, portraits by Hambling will be presented at the Italian gallery Thomas Brambilla. The artist’s “Wave Series” is also currently being exhibited in the group show “Summer Exhibition” at Marlborough London, which runs through September 10th.' type='textPrimary' />
+                    <Typography content='The excitement surrounding this exhibition delighted longtime fans of the expressive painter while inspiring a legion of new devotees—a trend matched by Neel’s strengthening position in the art market, especially in the past year. In May, Neel’s 1966 canvas Dr. Finger’s Waiting Room roused a flurry of bids from the United States, Asia, and Europe at Christie’s New York, where it ultimately sold for just over $3 million, breaking both its high estimate and the artist’s auction record. Just hours earlier at Sotheby’s New York, Neel’s double portrait Henry and Sally Hope (1977), depicting an art historian and his wife, sold for just under $1.6 million, notching her third-highest auction result.' type='textPrimary' />
+                    <Typography content='The demand for Maggi Hambling’s evocative portraits and exuberant depictions of seascapes and landscapes surged this past week, when the number of collectors inquiring on her work increased more than tenfold from the week before. The British artist, esteemed for her whirling, gestural paintings and bold public sculptures, has seen a consistent wave of interest in her work that has accelerated in the past few years. This recent uptick in interest  is consistent with Hambling’s career trajectory, which has been punctuated by a flurry of public commissions, institutional recognition, and secondary-market demand.' type='textPrimary' />
+                </div>
+                <div className='content-page_actions'>
+                    <div className='content-page_actions-likes'>
+                        <IconButton onClick={() => console.log('palecvverh')}>
+                            <img src={thumbsUp} alt='thumbsUp' />
+                        </IconButton>
+                        <IconButton onClick={() => console.log('palecvniz')}>
+                            <img src={thumbsDown} alt='thumbsDown' />
+                        </IconButton>
+                    </div>
+                    <IconButton onClick={() => console.log('favorites')
+                    }>
+                        <div className='content-page_actions-favorites'>
+                            <img src={favorites} alt='favorites' />
+                            Add to favorites
+                        </div>
+                    </IconButton>
+                </div>
+            </div>
+            <div className='content-page_transitions'>
+                <IconButton onClick={() => console.log('arrowPrev')
+                }>
+                    <div>
+                        <img src={arrowPrev} alt='arrowPrev' />
+                        <div className='content-page_transitions-texts-prev'>
+                            <div className='content-page_transitions-title'>
+                                Prev
+                            </div>
+                            <div className='content-page_transitions-text'>
+                                10 Things to Know About Salvador Dalí
+                            </div>
+                        </div>
+                    </div>
+                </IconButton>
+                <IconButton onClick={() => console.log('arrowNext')
+                }>
+                    <div className='content-page_transitions-box'>
+                        <div className='content-page_transitions-texts-next'>
+                            <div className='content-page_transitions-title'>
+                                Next
+                            </div>
+                            <div className='content-page_transitions-text'>
+                                8 Beautiful Villas Belonging to Artists You Need to See
+                            </div>
+                            <img src={arrowNext} alt="arrowNext" />
+                        </div>
+                    </div>
+                </IconButton>
+            </div>
+        </>
     )
 };
